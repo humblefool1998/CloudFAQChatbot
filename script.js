928f1b4044f151cd1eea3f3a88730a3421eb12b9
Raktim Chatterjee <chatterjeeraktim1998@gmail.com>
2022-01-30 23:21:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-     const styleSet = window.WebChat.createStyleSet({
+/*
+     POST /knowledgebases/8acd60ca-96c0-4928-aedc-50af1d8c8202/generateAnswer
+     Host: https://raktimqnaservice.azurewebsites.net/qnamaker
+     Authorization: EndpointKey fb2aba76-e8e7-4e6f-9db6-b673754ad61b
+     Content-Type: application/json
+     {"question":"<Your question>"}
+*/   
+const styleSet = window.WebChat.createStyleSet({
          bubbleBackground: 'rgba(0, 0, 255, .1)',
          bubbleFromUserBackground: 'rgba(0, 255, 0, .1)',
          rootHeight: '100%',
@@ -18,6 +25,9 @@
     window.WebChat.renderWebChat({
       directLine: window.WebChat.createDirectLine({
           token: 'ztbNafDL1qc.2fDYf6JC8e5fjpp3jeTi9aI4npXef52SKyQqOd6I5eY'}),   //QnA Maker generated token
+         /*
+         <iframe src='https://webchat.botframework.com/embed/raktimqnaservice-bot?s=YOUR_SECRET_HERE'  style='min-width: 400px; width: 100%; min-height: 500px;'></iframe>
+         */
       styleSet,
       styleOptions: avatarOptions
     }, document.getElementById('webchat'));
